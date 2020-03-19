89f1b29e06eef9339571a1f0ccde7641b569ccad
mhbahmani <mhbahmani79@gmail.com>
2020-03-19 21:26:03 +0330

    add reading ssids from file feature

--- connect.sh
+++ connect.sh
@@ -1,4 +1,18 @@
-curl -Ls -d "username=username&password=password" -X POST https://net2.sharif.edu/login
+nmcli radio wifi on
+sleep 2
+nmcli device wifi rescan
 
+login_page_url=https://net2.sharif.edu/login
+ssids="ssids.txt"
 
-#TODO: reading wifi ssids from a file
+while read ssid; do
+	nmcli device wifi connect $ssid
+	connection=$(wget -q --spider --timeout=5 --tries=1 $login_page_url; echo $?)
+	if [ $connection -eq 0 ]
+	then
+		echo Successfully connected
+		curl -Ls -d "username=username&password=password" -X POST $login_page_url
+		break
+	fi
+	done < $ssids
+
