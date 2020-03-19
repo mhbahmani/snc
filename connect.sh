8aebd05ffa222ade5fd2184b21cc50077ec153e7
mhbahmani <mhbahmani79@gmail.com>
2020-03-20 01:23:15 +0330

    add reading username and password from a file remove related TODO

--- connect.sh
+++ connect.sh
@@ -4,18 +4,22 @@ nmcli device wifi rescan
 
 login_page_url=https://net2.sharif.edu/login
 ssids="ssids.txt"
+username_password="username_password.txt"
+
+username=$(awk '/username/ {split($1,a,"="); print a[2]}' $username_password)
+password=$(awk '/password/ {split($1,a,"="); print a[2]}' $username_password)
+
+echo $username $password
 
 while read ssid; do
 	nmcli device wifi connect $ssid
 	connection=$(wget -q --spider --timeout=5 --tries=1 $login_page_url; echo $?)
-	if [ $connection -eq 0 ]
-	then
+	if [ $connection -eq 0 ]; then
 		echo Successfully connected
-		curl -Ls -d "username=username&password=password" -X POST $login_page_url
+		curl -Ls -d "username=$username&password=$password" -X POST $login_page_url
 		break
 	fi
 	done < $ssids
 
-#TODO: reading username and password from username_password file
 #TODO: add a command to adding new ssid to wifi_ssids file
 #TODO: add a command to initializing username_password.txt file
