fd02971577a2de51397608cb57eb45fa3166365a
mhbahmani <mhbahmani79@gmail.com>
2020-03-24 00:30:52 +0430

    improve init command

--- connect.sh
+++ connect.sh
@@ -2,7 +2,7 @@ login_page_url=https://net2.sharif.edu/login
 ssids="ssids.txt"
 username_password="username_password.txt"
 
-if test "$1" = "c" -o "$1" = "connect"; then
+if test "$1" = "connect" -o "$1" = "c"; then
 
 	nmcli radio wifi on
 	sleep 2
@@ -44,11 +44,17 @@ elif test  "$1" = "newmodem" -o "$1" = "n"; then
 	fi
 
 elif test "$1" = "init" -o "$1" = "i"; then
-	if [ -z $3 ]; then 
-		echo "please type both username and password"
-	else
-		encrypted_password=$(echo $3 | openssl enc -aes-128-cbc -iter 29 -a -salt -pass pass:asdffdsa)
-		echo -e "username:$2\npassword:$encrypted_password" > $username_password
+	if [ -z $2 ]; then
+		read -p "sharif-id username:" username
+		read -s -p "sharif-id password:" password
+		echo
+	elif [ -z $3 ]; then
+		echo "$2 assumed as your net2 username"
+		username=$2
+		read -s -p "sharif-id password:" password
 	fi
+
+	encrypted_password=$(echo $password | openssl enc -aes-128-cbc -iter 29 -a -salt -pass pass:asdffdsa)
+	echo -e "username:$username\npassword:$encrypted_password" > $username_password
 fi
 
