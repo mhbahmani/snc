78493307fe3c030f946684859141c3d3ecda7ff7
mhbahmani <mhbahmani79@gmail.com>
2020-03-24 17:24:23 +0430

    change connection procedure to connecting to strongest modem

--- connect.sh
+++ connect.sh
@@ -1,6 +1,6 @@
 login_page_url=https://net2.sharif.edu/login
-ssids="ssids.txt"
-username_password="username_password.txt"
+favorite_ssids_file="ssids.txt"
+username_password_file="username_password.txt"
 
 if test "$1" = "connect" -o "$1" = "c"; then
 
@@ -8,21 +8,27 @@ if test "$1" = "connect" -o "$1" = "c"; then
 	sleep 2
 	nmcli device wifi rescan
 
-	if test -f $username_password; then
-		username=$(awk '/username/ {split($1,a,":"); print a[2]}' $username_password)
-		encrypted_password=$(awk '/password/ {split($1,a,":"); print a[2]}' $username_password)
+
+	if test -f $username_password_file; then
+		username=$(awk '/username/ {split($1,a,":"); print a[2]}' $username_password_file)
+		encrypted_password=$(awk '/password/ {split($1,a,":"); print a[2]}' $username_password_file)
 		password=$(echo $encrypted_password | openssl enc -aes-128-cbc -iter 29 -a -d -salt -pass pass:asdffdsa)
 	else
 		echo "please first set your net2 username and password using init command"
 		exit
 	fi
 
-	if test ! -f $ssids; then
+	if test -f $favorite_ssids_file; then
+		online_favorite_ssids=$(nmcli device wifi list | grep -wi -f $favorite_ssids_file | cut -d '*' -f 2 | awk '{print $1}')
+		ssids=($online_favorite_ssids)
+	else
 		echo "There is no modem id in your list. Add one with newmodem command"
 		exit
 	fi
 
-	while read ssid; do
+	for ssid in "${ssids[@]}"
+	do
+		echo $ssid
 		nmcli device wifi connect $ssid > /dev/null 2>&1
 		connection=$(ping -q -w 5 -c 1 $login_page_url > /dev/null 2>&1 && echo 0 || echo 1)
 		if [ $connection -eq 0 ]; then
@@ -30,7 +36,7 @@ if test "$1" = "connect" -o "$1" = "c"; then
 			curl -Ls -d "username=$username&password=$password" -X POST $login_page_url
 			break
 		fi
-		done < $ssids
+	done
 
 elif test  "$1" = "newmodem" -o "$1" = "n"; then
 	if [ -z $2 ]; then
@@ -39,7 +45,7 @@ elif test  "$1" = "newmodem" -o "$1" = "n"; then
 		new_ssids=($@)
 		for (( i=1; i<=($#-1); i++ ))
 		do  
-   			echo "${new_ssids[$i]}" >> $ssids
+   			echo "${new_ssids[$i]}" >> $favorite_ssids_file
 		done
 	fi
 
@@ -55,6 +61,6 @@ elif test "$1" = "init" -o "$1" = "i"; then
 	fi
 
 	encrypted_password=$(echo $password | openssl enc -aes-128-cbc -iter 29 -a -salt -pass pass:asdffdsa)
-	echo -e "username:$username\npassword:$encrypted_password" > $username_password
+	echo -e "username:$username\npassword:$encrypted_password" > $username_password_file
 fi
 
