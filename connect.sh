cd596e8e5d38c4aec7557df4ab15be00737a82c0
mhbahmani <mhbahmani79@gmail.com>
2020-03-24 18:09:45 +0430

    handle each command in a function

--- connect.sh
+++ connect.sh
@@ -2,65 +2,99 @@ login_page_url=https://net2.sharif.edu/login
 favorite_ssids_file="ssids.txt"
 username_password_file="username_password.txt"
 
-if test "$1" = "connect" -o "$1" = "c"; then
+load_username_password(){
+        if test -f $username_password_file;
+        then
+                username=$(awk '/username/ {split($1,a,":"); print a[2]}' $username_password_file)
+                encrypted_password=$(awk '/password/ {split($1,a,":"); print a[2]}' $username_password_file)
+                password=$(echo $encrypted_password | openssl enc -aes-128-cbc -iter 29 -a -d -salt -pass pass:asdffdsa)
+        else
+                echo "please first set your net2 username and password using init command"
+                exit
+        fi
+}
 
+load_favorite_ssids(){
+        if test -f $favorite_ssids_file;
+        then
+                online_favorite_ssids=$(nmcli device wifi list | grep -wi -f $favorite_ssids_file | cut -d '*' -f 2 | awk '{print $1}')
+                ssids=($online_favorite_ssids)
+        else
+                echo "There is no modem id in your list. Add one with newmodem command"
+                exit
+        fi
+}
+
+try_connecting_to_modems(){
+	for ssid in "${ssids[@]}"
+        do      
+                nmcli device wifi connect $ssid > /dev/null 2>&1
+                connection=$(ping -q -w 5 -c 1 $login_page_url > /dev/null 2>&1 && echo 0 || echo 1)
+                if [ $connection -eq 0 ];
+                then    
+                        echo Successfully connected to $ssid
+                        curl -Ls -d "username=$username&password=$password" -X POST $login_page_url
+                        break
+                fi
+        done
+
+}
+
+connect(){
 	nmcli radio wifi on
 	sleep 2
 	nmcli device wifi rescan
 
+	load_username_password
 
-	if test -f $username_password_file; then
-		username=$(awk '/username/ {split($1,a,":"); print a[2]}' $username_password_file)
-		encrypted_password=$(awk '/password/ {split($1,a,":"); print a[2]}' $username_password_file)
-		password=$(echo $encrypted_password | openssl enc -aes-128-cbc -iter 29 -a -d -salt -pass pass:asdffdsa)
-	else
-		echo "please first set your net2 username and password using init command"
-		exit
-	fi
+	load_favorite_ssids
 
-	if test -f $favorite_ssids_file; then
-		online_favorite_ssids=$(nmcli device wifi list | grep -wi -f $favorite_ssids_file | cut -d '*' -f 2 | awk '{print $1}')
-		ssids=($online_favorite_ssids)
-	else
-		echo "There is no modem id in your list. Add one with newmodem command"
-		exit
-	fi
+	try_connecting_to_modems
+}
 
-	for ssid in "${ssids[@]}"
-	do
-		echo $ssid
-		nmcli device wifi connect $ssid > /dev/null 2>&1
-		connection=$(ping -q -w 5 -c 1 $login_page_url > /dev/null 2>&1 && echo 0 || echo 1)
-		if [ $connection -eq 0 ]; then
-			echo Successfully connected
-			curl -Ls -d "username=$username&password=$password" -X POST $login_page_url
-			break
-		fi
-	done
-
-elif test  "$1" = "newmodem" -o "$1" = "n"; then
-	if [ -z $2 ]; then
+add_modem(){
+	if [ -z $1 ];
+	then
 		echo "please type modem ssid"
 	else
 		new_ssids=($@)
+		echo $#
 		for (( i=1; i<=($#-1); i++ ))
-		do  
+		do
    			echo "${new_ssids[$i]}" >> $favorite_ssids_file
 		done
 	fi
+}
 
-elif test "$1" = "init" -o "$1" = "i"; then
-	if [ -z $2 ]; then
+initialize(){
+	if [ -z $1 ];
+	then
 		read -p "sharif-id username:" username
 		read -s -p "sharif-id password:" password
 		echo
-	elif [ -z $3 ]; then
-		echo "$2 assumed as your net2 username"
-		username=$2
+	elif [ -z $1 ];
+	then
+		echo "$1 assumed as your net2 username"
+		username=$1
 		read -s -p "sharif-id password:" password
 	fi
 
 	encrypted_password=$(echo $password | openssl enc -aes-128-cbc -iter 29 -a -salt -pass pass:asdffdsa)
 	echo -e "username:$username\npassword:$encrypted_password" > $username_password_file
-fi
+}
+
+case $1 in
+        connect | c)
+		connect
+		;;  
+        init | i)
+		initialize $2 $3 
+                ;;  
+        newmodem | n)
+                add_modem $@
+                ;;  
+        --help | -h)
+                echo "help"
+                ;;  
+esac
 
