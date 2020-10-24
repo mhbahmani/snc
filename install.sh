60135a04b8236bbf718d4aeff74a8706d62800fd
mhbahmani <mhbahmani79@gmail.com>
2020-10-24 15:01:29 +0330

    add curl install to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+declare -A osInfo;
+osInfo[/etc/debian_version]="sudo apt-get install -y"
+osInfo[/etc/alpine-release]="sudo apk --update add"
+osInfo[/etc/centos-release]="sudo yum install -y"
+osInfo[/etc/fedora-release]="sudo dnf install -y"
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi
+done
+
+curl_inst=0
+[ $(curl --version | grep -Eci 'release-date') = 0 ] && curl_inst=1
+
+if [ $curl_inst = 1 ]; then
+    if [ -z $package_manager ]; then
+        echo "unknown linux distro".
+        echo "try installing curl with your package manager"
+        echo "then run the installation command again"
+        echo "exiting."
+        exit
+    fi
+    [ curl_intst = 1 ] && $package_manager curl
+fi
+
 main_destination=/usr/local/bin/snc
 sudo rm -rf $main_destination 
 
@@ -19,7 +46,7 @@ echo "snc successfully installed."
 echo "enjoy!"
 echo
 
-sleep 2
+sleep 1
 clear
 
 echo "\`snc\` connects to sharif modems and log you in to net2"
